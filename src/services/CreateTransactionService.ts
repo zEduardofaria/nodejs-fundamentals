0b86a5f413505dc520b0d25a8fc761ef33344a08
Eduardo Faria <z.eduardofaria@gmail.com>
2020-05-14 15:59:00 -0300

    Transaction create and list

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  type: string;
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,9 +14,7 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
-  }
+  public execute({ title, value, type }: Request): Transaction {}
 }
 
 export default CreateTransactionService;
